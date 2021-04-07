4b51daabc462eedc8a4ec2ca3732a8737679356d
singchengtseng <singcheng.tseng@gmail.com>
2021-04-07 01:40:34 -0700

    main.cpp

--- midterm/main.cpp
+++ midterm/main.cpp
@@ -8,18 +8,25 @@ InterruptIn  pin1(D5);
 InterruptIn  pin2(D6);
 InterruptIn  pin3(D10);
 uLCD_4DGL uLCD(D1, D0, D2);
+AnalogOut  aout(PA_4);
 float sel_tmp=0.125;
 float sel=0;
 int mod=0;
 void up()
 {
-    if(sel_tmp!=1&&mod==0)
-    sel_tmp=sel_tmp*2;
+    if(sel_tmp!=1)
+    {
+        mod=0;
+        sel_tmp=sel_tmp*2;
+    }
 }
 void down()
 {
-    if(sel_tmp!=0.125&&mod==0)
-    sel_tmp=sel_tmp/2;
+    if(sel_tmp!=0.125)
+    {
+        mod=0;
+        sel_tmp=sel_tmp/2;
+    }
 }
 void sele()
 {
@@ -33,15 +40,24 @@ int main(void)
     pin3.rise(&sele);
     while(1)
     {
-        if(mod==0)
-        {
             uLCD.printf("%f\n",sel_tmp);
-            ThisThread::sleep_for(500ms);
-            uLCD.cls();
+            ThisThread::sleep_for(1000ms);
+        
+        if(mod==1)
+        {
+        for (float i = 0.0; i < 1.0; i += (0.0125/sel)) {
+            aout = i;
+            ThisThread::sleep_for(1ms);
+      }
+        for (int t=0;t<80/sel;t=t+1){
+            aout =1;
+            ThisThread::sleep_for(1ms);
         }
-        else if(mod==1)
-            uLCD.printf("%f\n",sel);
-            ThisThread::sleep_for(500ms);
-            uLCD.cls();
+        for (float i = 1.0; i > 0; i -= (0.0125/sel)) {
+            aout = i;
+            ThisThread::sleep_for(1ms);
+      }
+        }
+        uLCD.cls();
     }
 }
